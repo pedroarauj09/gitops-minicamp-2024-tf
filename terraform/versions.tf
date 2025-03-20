2e82ad08278a15b3a30bbb5060964795fc1961ae
Pedro Araújo <61950266+pedroarauj09@users.noreply.github.com>
2025-03-20 17:13:40 +0000

    add dynamo and s3

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,8 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket = "gitops-tf-s3-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "gitops-tf-dynamo-backend"
+  }
+}
